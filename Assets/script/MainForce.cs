c2a173523d40fc48bf5cb2f673dda607db9864e7
ookami1204 <corocorobox12@gmail.com>
2022-10-25 15:55:41 +0900

    force完成

--- Assets/script/MainForce.cs
+++ Assets/script/MainForce.cs
@@ -20,8 +20,12 @@ public class MainForce : MonoBehaviour
     [SerializeField]
     float backSpeed;
 
-    bool Ispush;
-    Coroutine backCoroutine;
+    bool IsPush;
+    bool IsBack;
+    bool IsRight = true;
+
+    Coroutine moveVertical;
+    Coroutine moveWidth;
 
     void Start()
     {
@@ -30,7 +34,11 @@ public class MainForce : MonoBehaviour
 
     void Update()
     {
-        if(Ispush == false)
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, mainCamera.transform.position.x - 8f,
+                                                                            mainCamera.transform.position.x + 8f),
+                                         Mathf.Clamp(transform.position.y, mainCamera.transform.position.y - 4.5f,
+                                                                            mainCamera.transform.position.y + 4.5f));
+        if(IsPush == false)
         {
             if (transform.position.x > player.transform.position.x)
             {
@@ -44,14 +52,15 @@ public class MainForce : MonoBehaviour
         if (Input.GetButtonDown("ShotForce"))
         {
             rb.velocity = Vector2.zero;
-            if(Ispush == true)
+            if(IsPush == true)
             {
+                StopAllCoroutines();
                 StartCoroutine(Back());
             }
             else
             {
-                Ispush = true;
-                if (transform.position.x > player.transform.position.x)
+                IsPush = true;
+                if (IsRight)
                 {
                     rb.AddForce(Vector2.right * speed, ForceMode2D.Impulse);
                 }
@@ -60,22 +69,57 @@ public class MainForce : MonoBehaviour
                     rb.AddForce(Vector2.left * speed, ForceMode2D.Impulse);
                 }
             }
+            moveVertical = StartCoroutine(MoveVertical());
         }
     }
 
     IEnumerator Back()
     {
-        while(Ispush == true)
+        IsBack = true;
+        while(IsPush == true)
         {
             yield return new WaitForEndOfFrame();
             rb.velocity = (player.transform.position - transform.position).normalized * backSpeed;
         }
     }
+    IEnumerator MoveWidth()
+    {
+        float movePos;
+        if (IsRight)
+        {
+            movePos = mainCamera.transform.position.x + 7 - transform.position.x;
+        }
+        else
+        {
+            movePos = mainCamera.transform.position.x - 7 - transform.position.x;
+        }
+        while (movePos > 0.25f)
+        {
+            yield return new WaitForEndOfFrame();
+            rb.velocity = new Vector2(movePos, rb.velocity.y).normalized * speed;
+        }
+        rb.velocity = Vector2.zero;
+    }
+    IEnumerator MoveVertical()
+    {
+        while (IsPush)
+        {
+            yield return new WaitForSeconds(4);
+            while (Mathf.Abs(player.transform.position.y - transform.position.y) > 0.25f)
+            {
+                yield return new WaitForEndOfFrame();
+                rb.velocity = new Vector2(rb.velocity.x, player.transform.position.y - transform.position.y).normalized * speed;
+            }
+            rb.velocity = Vector2.zero;
+        }        
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.CompareTag("Enemy") && IsBack == false)
         {
+            rb.velocity = Vector2.zero;
+            moveWidth = StartCoroutine(MoveWidth());
             Instantiate(effect, new Vector2(transform.position.x + 0.5f, transform.position.y), Quaternion.identity);
         }
     }
@@ -83,15 +127,18 @@ public class MainForce : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         rb.velocity = Vector2.zero;
-        if (collision.gameObject.CompareTag("Player") && Ispush == true)
+        if (collision.gameObject.CompareTag("Player") && IsPush == true)
         {
-            Ispush = false;
+            IsBack = false;
+            IsPush = false;
             if (transform.position.x > player.transform.position.x)
             {
+                IsRight = true;
                 transform.position = new Vector2(player.transform.position.x + 0.8f, player.transform.position.y);
             }
             else
             {
+                IsRight = false;
                 transform.position = new Vector2(player.transform.position.x - 0.8f, player.transform.position.y);
             }
         }
